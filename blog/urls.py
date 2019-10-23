ef5b001a0a4b9a53eb938dd73575faf484536d49
Yongun Seong <nevivurn@riseup.net>
2019-10-24 03:41:39 +0900

    Finish auth

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from blog import views
 
+# TODO: REMOVE, ONLY FOR DEBUGGING
+#from django.views.decorators.csrf import csrf_exempt
+
+# TODO: used during testing, since it needs to be actually on
+def csrf_exempt(i): return i
+
 urlpatterns = [
-    path('signup', views.signup, name='signup'),
-    path('token', views.token, name='token'),
+    path('signup', csrf_exempt(views.signup), name='signup'),
+    path('signin', csrf_exempt(views.signin), name='signin'),
+    path('signout', csrf_exempt(views.signout), name='signout'),
+    path('token', csrf_exempt(views.token), name='token'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,62 @@
 from django.http import HttpResponse, HttpResponseNotAllowed
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
 from django.views.decorators.csrf import ensure_csrf_cookie
 import json
-
+from json.decoder import JSONDecodeError
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+
+    try:
         req_data = json.loads(request.body.decode())
         username = req_data['username']
         password = req_data['password']
-        User.objects.create_user(username, password)
-        return HttpResponse(status=201)
-    else:
+    except (KeyError, JSONDecodeError):
+        return HttpResponse(status=400)
+    # extra keys
+    if req_data.keys() - {'username', 'password'}:
+        return HttpResponse(status=400)
+
+    try:
+        User.objects.create_user(username=username, password=password)
+    except IntegrityError:
+        return HttpResponse(status=400)
+
+    return HttpResponse(status=201)
+
+def signin(request):
+    if request.method != 'POST':
         return HttpResponseNotAllowed(['POST'])
 
+    try:
+        req_data = json.loads(request.body.decode())
+        username = req_data['username']
+        password = req_data['password']
+    except (KeyError, JSONDecodeError):
+        return HttpResponse(status=400)
+    # extra keys
+    if req_data.keys() - {'username', 'password'}:
+        return HttpResponse(status=400)
+
+    user = authenticate(request, username=username, password=password)
+    if user == None:
+        return HttpResponse(status=401)
+
+    login(request, user)
+    return HttpResponse(status=204)
+
+def signout(request):
+    if request.method != 'GET':
+        return HttpResponseNotAllowed(['GET'])
+
+    if not request.user.is_authenticated:
+        return HttpResponse(status=401)
+
+    logout(request)
+    return HttpResponse(status=204)
 
 @ensure_csrf_cookie
 def token(request):
